8e54f1c1a7e8834f42ae874a11c4f5a0fcf02a1c
liangman <liangman@obook.com.cn>
2020-05-13 17:04:18 +0800

    新文件：   media/__init__.py 	修改：     ota/wsgi.py

--- ota/wsgi.py
+++ ota/wsgi.py
@@ -9,6 +9,15 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
+from os.path import dirname,abspath
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+import sys
+sys.path.insert(0,PROJECT_DIR)
+
+os.environ['HTTPS'] = "on"
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ota.settings")
